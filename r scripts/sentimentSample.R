669e09be5aeb211e2b45b6f117454756afb60137
JakobFehle <jakob.fehle@stud.uni-regensburg.de>
2017-12-11 19:19:50 +0100

    Veränderung des Twitter_Data Basisdatensatzes

--- r scripts/sentimentSample.R
+++ r scripts/sentimentSample.R
@@ -3,16 +3,24 @@ require(dplyr)
 library(readr)
 source("r scripts/cleanTweetText.R")
 
-twitter_data<-read_csv("D:/GitHub/CSS-WS17-18/data/twitter_data.csv", 
+twitter_data<-read_csv("data/twitter_data.csv", 
                         locale = locale())
 
-twitter_data_sentiment<-read_delim("D:/GitHub/CSS-WS17-18/data/Twitter_Sentiment_TrainData2.csv", 
+twitter_data_sentiment<-read_delim("data/Twitter_Sentiment_TrainData2.csv", 
                                                                    ";", escape_double = FALSE, trim_ws = TRUE)
 twitter_data_sentiment<-twitter_data_sentiment%>%select(-c(Nummer))
 
+# Aufbereiten Twitter Datensatz
+twitter_data<-twitter_data%>%filter(lang=="de" | lang == "da")%>%filter(text!=".*\\.{3}")
+twitter_data<-twitter_data[!grepl("\\.\\.\\.", twitter_data$text),]
+
+
+# Aufbereiten für Random Sample
 twitter_data<-twitter_data%>%mutate(sentimentScore=NA)
 twitter_data$text<-gsub(","," ",twitter_data$text)
 twitter_data$text<-gsub(";"," ",twitter_data$text)
+
+# Random Sample
 twitter_data_frac<-twitter_data%>%sample_n(1000, replace=FALSE)
 twitter_data_frac<-twitter_data_frac%>%select(ID,sentimentScore,text)
 
