9ebb7e23f77c20882435c3278aab260b3faabdee
JakobFehle <jakob.fehle@stud.uni-regensburg.de>
2017-12-27 00:21:06 +0100

    Hinzufügen des Twitter Datensatzes inkl erzeugten Sentiments mit den vorhandenen Lexika

--- r scripts/predictSentiment.R
+++ r scripts/predictSentiment.R
@@ -25,6 +25,40 @@ write.csv(twitter_data, "twitter data/twitterDataWithSentiment.csv", row.names =
 
 as.numeric(unlist(predict))
 
+# Dataset is too big -> split
 twitter_data1<-twitter_data[1:120000,]
+twitter_data2<-twitter_data[120001:255307,]
 twitter_data_cleaned1<-twitter_data_cleaned[1:120000,]
-predict1<-predict(ridge_5lvl,twitter_data_cleaned1$text)
+twitter_data_cleaned2<-twitter_data_cleaned[120001:255307,]
+
+
+twitter_data1$ridge5<-as.numeric(unlist(predict(ridge_5lvl,twitter_data_cleaned1$text)))
+twitter_data2$ridge5<-as.numeric(unlist(predict(ridge_5lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$ridge3<-as.numeric(unlist(predict(ridge_3lvl,twitter_data_cleaned1$text)))
+twitter_data2$ridge3<-as.numeric(unlist(predict(ridge_3lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$ridge2<-as.numeric(unlist(predict(ridge_2lvl,twitter_data_cleaned1$text)))
+twitter_data2$ridge2<-as.numeric(unlist(predict(ridge_2lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$enet5<-as.numeric(unlist(predict(enet_5lvl,twitter_data_cleaned1$text)))
+twitter_data2$enet5<-as.numeric(unlist(predict(enet_5lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$enet3<-as.numeric(unlist(predict(enet_3lvl,twitter_data_cleaned1$text)))
+twitter_data2$enet3<-as.numeric(unlist(predict(enet_3lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$enet2<-as.numeric(unlist(predict(enet_2lvl,twitter_data_cleaned1$text)))
+twitter_data2$enet2<-as.numeric(unlist(predict(enet_2lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$lasso5<-as.numeric(unlist(predict(lasso_5lvl,twitter_data_cleaned1$text)))
+twitter_data2$lasso5<-as.numeric(unlist(predict(lasso_5lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$lasso3<-as.numeric(unlist(predict(lasso_3lvl,twitter_data_cleaned1$text)))
+twitter_data2$lasso3<-as.numeric(unlist(predict(lasso_3lvl,twitter_data_cleaned2$text)))
+
+twitter_data1$lasso2<-as.numeric(unlist(predict(lasso_2lvl,twitter_data_cleaned1$text)))
+twitter_data2$lasso2<-as.numeric(unlist(predict(lasso_2lvl,twitter_data_cleaned2$text)))
+
+twitter_data_WS<-rbind(twitter_data1,twitter_data2)
+
+write.csv(twitter_data_WS, "twitter data/twitterDataWithSentiment.csv", sep=",",col.names = TRUE, row.names = FALSE)
